a625bd6ea9b9047fc52edbaa3ffd48e691562661
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2022-11-04 19:34:38 -0300

    adicionando responsividade

--- main.js
+++ main.js
@@ -8,9 +8,12 @@ function createGame(player1, hour, player2){
   `
 }
 
+let delay = -0.4
+
 function createCard(date, day, games) {
+  delay = delay + 0.4;
   return `
-  <div class="card">
+  <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
         <ul>
           ${games}
@@ -19,13 +22,7 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector('#app').innerHTML = `
-<header>
-      <img src="./assets/logo.svg" alt="">
-    </header>
-    <main id="cards">
-      ${createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia'))}
-      ${createCard('28/11', 'segunda', createGame('switzerland', '13:00', 'brazil') + createGame('uruguay', '16:00', 'portugal'))}
-      ${createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))}
-    </main>
-`
+document.querySelector('#cards').innerHTML =
+      createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia')) +
+      createCard('28/11', 'segunda', createGame('switzerland', '13:00', 'brazil') + createGame('uruguay', '16:00', 'portugal')) +
+      createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))
